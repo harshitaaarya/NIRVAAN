e2ed1986d6cf38dbd3e27ef6b9032cedd2563a06
Harshita aarya <harshitaaaarya@gmail.com>
2025-11-10 23:51:26 +0530

    Add highlightTile function to scale and shadow tiles

---
+++ script.js
@@ -0,0 +1,9 @@
+function highlightTile(tile) {
+  const all = document.querySelectorAll('.tile');
+  all.forEach(t => {
+    t.style.transform = "scale(1)";
+    t.style.boxShadow = "none";
+  });
+  tile.style.transform = "scale(1.1)";
+  tile.style.boxShadow = "0 8px 20px rgba(0,0,0,0.3)";
+}
